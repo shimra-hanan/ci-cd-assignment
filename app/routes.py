c7d31de9897deeab9dd135298291338f3245eb7c
RadiyahHassan <123581901+RadiyahHassan@users.noreply.github.com>
2025-07-28 15:44:41 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,10 @@ from app.models import Menu
 def home():
 	return jsonify({ "status": "ok" })
 
+@app.route('/version')
+def version():
+    return jsonify({ "version": "1.0.1", "build": "assignment-ci-cd-v1" }), 200
+
 @app.route('/menu')
 def menu():
     today = Menu.query.first()
